df3fc9bc28bf33b9702b493fcfcc428f164994ac
Vatsal Mandaliya <vmc9152@gmail.com>
2023-11-03 01:47:48 +0530

    removed I am

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -42,7 +42,7 @@ const Main = () => {
 
       <div className='w-full h-screen absolute top-0 left-0 bg-white/50 '>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center '>
-          <h1 className='sm:text-5xl  text-4xl font-bold  text-gray-800 '>I am Vatsal Mandaliya</h1>
+          <h1 className='sm:text-5xl  text-4xl font-bold  text-gray-800 '>Vatsal Mandaliya</h1>
           <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
             I am a
             <TypeAnimation
